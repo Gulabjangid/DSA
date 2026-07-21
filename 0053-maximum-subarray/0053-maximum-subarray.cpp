929c2ee68e64a32c1c77999b113bf251d2dafa2c
Gulab Jangid <gulabjangid309@gmail.com>
2026-07-21 14:50:28 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.79%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int count=0;
+        int maxsum=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            count+=nums[i];
+            
+            maxsum=max(maxsum,count);
+            if(count<0){
+                count=0;
+            }
+            
+        }
+        return maxsum;
+        
+    }
+};
