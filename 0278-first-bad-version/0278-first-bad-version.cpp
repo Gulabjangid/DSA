44a126025a463048b5113fdc97c72e23c64f5b3a
Gulab Jangid <gulabjangid309@gmail.com>
2026-07-26 09:32:00 +0530

    Time: 2 ms (57.73%), Space: 8 MB (5.27%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,22 +5,17 @@ class Solution {
 public:
     int firstBadVersion(int n) {
 
-        int a = 0;
-        int b = n;
-        int ans;
-
-        while (a < b) {
-            int mid = a + (b - a) / 2;
-
-            if(!isBadVersion(mid) && isBadVersion(mid-1) ){
-                ans=mid-1;
-            }
+        int left = 1;
+        int right = n;
+       
+        while (left < right) {
+            int mid = left + (right - left) / 2;
             if (isBadVersion(mid)) {
-                a = mid + 1;
+                right = mid;
             } else {
-                b = mid - 1;
+                left = mid + 1;
             }
         }
-        return ans;
+        return left;
     }
 };
