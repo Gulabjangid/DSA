dc0bd9ab16a804b2057302bc2b8fb05c9dfd6a7b
Gulab Jangid <gulabjangid309@gmail.com>
2026-08-15 14:10:46 +0530

    Time: 0 ms (100%), Space: 9.3 MB (90.25%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int secondHighest(string s) {
-
+        int frist = -1;
         int second = -1;
-        int largest = -1;
-
         for (char c : s) {
             if (isdigit(c)) {
-                int digit = c - '0';
+                int val = c - '0';
 
-                if (digit > largest) {
-                    second = largest;
-                    largest = digit;
-                }
-                 else if(digit > second && digit != largest){
-                    second = digit;
+                if (val > frist) {
+                    second = frist;
+                    frist = val;
+                } else if (val > second && val != frist) {
+                    second = val;
                 }
             }
         }
